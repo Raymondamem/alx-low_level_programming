9c0f52096eab718381f73f84059d758e2b097dba
Raymondamem <raymondamem525@gmail.com>
2023-03-21 20:45:31 -0700

    done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of a number
+ * @n: number to check
+ *
+ * Return: 1 if positive, 0 if zero, -1 if negative
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
