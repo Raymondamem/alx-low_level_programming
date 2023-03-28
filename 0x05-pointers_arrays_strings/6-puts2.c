51adea230ff1b8cb52ed79043eb34eb2e9dcecd2
Raymondamem <raymondamem525@gmail.com>
2023-03-28 12:26:48 -0700

    done

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* puts2 - printing function
+* @str: parameter to print
+* Return: void
+*/
+void puts2(char *str)
+{
+	int i = 0;
+	int length = 0;
+
+	while (*(str + length) != '\0')
+	{
+		length++;
+	}
+
+	while (i < length)
+	{
+		_putchar(*(str + i));
+		i += 2;
+	}
+	_putchar('\n');
+}
