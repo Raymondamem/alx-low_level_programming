bd13d9aed6eecc67d7e8830d4eea7e3882904b23
Raymondamem <raymondamem525@gmail.com>
2023-03-31 10:42:25 -0700

    done

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,18 +8,21 @@
  */
 char *leet(char *s)
 {
-	int i, j;
-	char letters[] = "aeotlAEOTL";
-	char numbers[] = "4307104310";
+	int i;
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 10; j++)
+		int j;
+		for (j = 0; letters[j] != '\0'; j++)
 		{
 			if (s[i] == letters[j])
-			s[i] = numbers[j];
+			{
+				s[i] = numbers[j];
+				break;
+			}
 		}
 	}
-
 	return (s);
 }
