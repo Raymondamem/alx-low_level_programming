57c5d7739701d48a6e39412892e04d217c557f98
Raymondamem <raymondamem525@gmail.com>
2023-04-03 22:15:21 -0700

    done

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * set_string
+ * @s: pointer
+ * @to: vaue 
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -15,4 +15,6 @@ char *_strstr(char *haystack, char *needle);
 
 void print_diagsums(int *a, int size);
 
+void set_string(char **s, char *to);
+
 #endif /* _MAIN_H_ */
