9121258e887464343c0ba09f1de33edc0eb2ec0c
Raymondamem <raymondamem525@gmail.com>
2023-04-05 21:06:27 -0700

    done

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: the string to check
+ *
+ * Return: 1 if s is a palindrome, 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	return (check_palindrome(s, 0, len - 1));
+}
+
+/**
+ * check_palindrome - recursive function to check if a string is a palindrome
+ * @s: the string to check
+ * @start: the starting index
+ * @end: the ending index
+ *
+ * Return: 1 if s is a palindrome, 0 otherwise
+ */
+int check_palindrome(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+
+	if (s[start] != s[end])
+		return (0);
+
+	return (check_palindrome(s, start + 1, end - 1));
+}
