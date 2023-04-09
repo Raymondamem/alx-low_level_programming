ac3603682349c56357332d04a0aba7282400f1a5
Raymondamem <raymondamem525@gmail.com>
2023-04-09 05:21:00 -0700

    done

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,11 +4,11 @@
  * main - Prints the number of arguments passed to the program.
  * @argc: Number of arguments passed to the program.
  * @argv: Array of pointers to the strings which are those arguments.
- *
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
+	argv[0] = "d";
 	printf("%d\n", argc - 1);
 	return (0);
 }
