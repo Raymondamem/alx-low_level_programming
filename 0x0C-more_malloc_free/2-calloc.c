db776b862fa926e9f4ca16a00d47e0810846dc92
Raymondamem <raymondamem525@gmail.com>
2023-04-14 07:55:07 -0700

    done

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _calloc - recreating calloc
+ * @nmemb: number of items
+ * @size: size of items
+ * Return: void pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	p = malloc(nmemb * size);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+	memset(p, 0, nmemb * size);
+	return (p);
+}
