78e4a615d1a64dbda0f8d69fb484a8a9ca5617df
Raymondamem <raymondamem525@gmail.com>
2023-04-20 12:50:15 -0700

    done

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,8 @@
+#ifndef _VAR_FUNC_H
+#define _VAR_FUNC_H
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+
+#endif /*VAR_FUNC_H*/
