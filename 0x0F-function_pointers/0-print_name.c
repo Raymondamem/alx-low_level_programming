567e9f6f3f240c953ae39194518ea8e5b94fe630
Raymondamem <raymondamem525@gmail.com>
2023-04-20 07:36:31 -0700

    done

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,9 @@
-#include <stdio.h>
 #include "function_pointers.h"
 
 /**
  * print_name - prints a name using the function pointed to by f
  * @name: name of the person
  * @f: pointer to the function to use
- *
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,10 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef _FUNCTION_POINTER_H
+#define _FUNCTION_POINTER_H
 
-void print_hello(void);
+#include <stddef.h>
 
-#endif /* FUNCTION_POINTERS_H */
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif /*FUNCTION_POINER_H*/
