d06db0fbaa20140bcc444541ad51da6880b0ff6b
Raymondamem <raymondamem525@gmail.com>
2023-04-22 12:19:10 -0700

    done

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,49 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_opcodes(void (*func)(void), int num_bytes);
-
 /**
  * main - prints the opcodes of its own main function
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: 0 on success, 1 if wrong number of arguments, 2 if negative num_bytes
+ * Return: 0 on success, 1 or 2 on error
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int num_bytes;
+	int i, n;
+	unsigned char *p;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return (1);
+		exit(1);
 	}
-
-	num_bytes = atoi(argv[1]);
-
-	if (num_bytes < 0)
+	n = atoi(argv[1]);
+	if (n < 0)
 	{
 		printf("Error\n");
-		return (2);
+		exit(2);
 	}
-
-	print_opcodes(main, num_bytes);
-
-	return (0);
-}
-
-/**
- * print_opcodes - prints the opcodes of a function
- * @func: pointer to the function to print opcodes of
- * @num_bytes: number of bytes to print
- */
-void print_opcodes(void (*func)(void), int num_bytes)
-{
-	int i;
-	unsigned char *opcodes = (unsigned char *)func;
-
-	for (i = 0; i < num_bytes; i++)
-		printf("%.2x ", opcodes[i]);
-
+	p = (unsigned char *)main;
+	for (i = 0; i < n; i++)
+	{
+		printf("%02x", p[i]);
+		if (i < n - 1)
+			printf(" ");
+	}
 	printf("\n");
+	return (0);
 }
