8fd4a39614fcf9e2e863332aefd2b0bfe722e56e
Raymondamem <raymondamem525@gmail.com>
2023-04-30 06:28:49 -0700

    done

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t count = 0;
+	listint_t *slow, *fast, *tmp;
+
+	if (h == NULL || *h == NULL)
+		return (0);
+	slow = *h;
+	fast = *h;
+
+	while (slow != NULL && fast != NULL && fast->next != NULL)
+	{
+		tmp = slow;
+		slow = slow->next;
+		fast = fast->next->next;
+		free(tmp);
+		count++;
+
+		if (slow == fast)
+		{
+			tmp = slow;
+			slow = slow->next;
+			free(tmp);
+			count++;
+			break;
+		}
+	}
+
+	while (slow != NULL)
+	{
+		tmp = slow;
+		slow = slow->next;
+		free(tmp);
+		count++;
+	}
+
+	*h = NULL;
+
+	return (count);
+}
