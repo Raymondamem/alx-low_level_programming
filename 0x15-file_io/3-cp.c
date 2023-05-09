938c7dffcc6a6bb7080244ab3f81dff1bb25f372
Raymondamem <raymondamem525@gmail.com>
2023-05-09 20:25:40 +0100

    done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,32 +7,11 @@
 
 #define BUFSIZE 1024
 
-/**
- * reduce - reduces the function
- */
-int reduce_2(int *fd_from, int *fd_to, int *n_read)
-{
-	if (*n_read == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close(*fd_from);
-		close(*fd_to);
-		exit(98);
-	}
-
-	if (close(*fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", *fd_from);
-		close(*fd_to);
-		exit(100);
-	}
-
-	if (close(*fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", *fd_to);
-		exit(100);
-	}
-}
+void check_argc(int argc);
+int open_file_from(char *file_from);
+int open_file_to(char *file_to);
+void copy_file(int fd_from, int fd_to, char *file_from, char *file_to);
+void close_files(int fd_from, int fd_to);
 
 /**
  * main - Entry point
@@ -44,42 +23,120 @@ int reduce_2(int *fd_from, int *fd_to, int *n_read)
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to;
-	ssize_t n_read;
-	char buf[BUFSIZE];
 
+	check_argc(argc);
+
+	fd_from = open_file_from(argv[1]);
+	fd_to = open_file_to(argv[2]);
+
+	copy_file(fd_from, fd_to, argv[1], argv[2]);
+
+	close_files(fd_from, fd_to);
+
+	return (0);
+}
+
+/**
+ * check_argc - Check if the number of arguments is correct
+ * @argc: Number of arguments
+ */
+void check_argc(int argc)
+{
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+}
 
-	fd_from = open(argv[1], O_RDONLY);
+/**
+ * open_file_from - Open the file to copy from
+ * @file_from: Name of the file to copy from
+ *
+ * Return: File descriptor of the opened file
+ */
+int open_file_from(char *file_from)
+{
+	int fd_from;
+
+	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
 
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	return (fd_from);
+}
+
+/**
+ * open_file_to - Open the file to copy to
+ * @file_to: Name of the file to copy to
+ *
+ * Return: File descriptor of the opened file
+ */
+int open_file_to(char *file_to)
+{
+	int fd_to;
+
+	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(fd_from);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 		exit(99);
 	}
+
+	return (fd_to);
+}
+
+/**
+ * copy_file - Copy the content of a file to another file
+ * @fd_from: File descriptor of the file to copy from
+ * @fd_to: File descriptor of the file to copy to
+ * @file_from: Name of the file to copy from
+ * @file_to: Name of the file to copy to
+ */
+void copy_file(int fd_from, int fd_to, char *file_from, char *file_to)
+{
+	ssize_t n_read;
+	char buf[BUFSIZE];
 
 	while ((n_read = read(fd_from, buf, BUFSIZE)) > 0)
 	{
 		if (write(fd_to, buf, n_read) != n_read)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 			close(fd_from);
 			close(fd_to);
 			exit(99);
 		}
 	}
+	if (n_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		close(fd_from);
+		close(fd_to);
+		exit(98);
+	}
+}
 
-	reduce_2(&fd_from, &fd_to, &n_read);
+/**
+ * close_files - Close two files
+ * @fd_from: File descriptor of the first file
+ * @fd_to: File descriptor of the second file
+ */
+void close_files(int fd_from, int fd_to)
+{
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		close(fd_to);
+		exit(100);
+	}
 
-	return (0);
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
 }
