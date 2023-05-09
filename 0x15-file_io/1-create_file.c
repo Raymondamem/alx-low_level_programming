3e2280815bc59e2a4c6447133a7d98a288e006d3
Raymondamem <raymondamem525@gmail.com>
2023-05-08 23:55:29 -0700

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	size_t len = 0;
+	ssize_t len = 0;
 
 	if (!filename)
 		return (-1);
@@ -29,6 +29,7 @@ int create_file(const char *filename, char *text_content)
 			len++;
 		if (write(fd, text_content, len) == -1)
 			return (-1);
+	}
 
 	close(fd);
 	return (1);
