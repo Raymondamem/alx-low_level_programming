3c345e5796e1ce16ac9267f19ca5e366e18799a1
Raymondamem <raymondamem525@gmail.com>
2023-05-09 00:04:05 -0700

    done

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include "main.h"
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t len = 0;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		while (text_content[len])
+			len++;
+		if (write(fd, text_content, len) == -1)
+			return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
