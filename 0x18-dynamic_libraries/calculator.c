ae741a154ff08cf91ec03ee5b2035498a5082c18
Raymondamem <raymondamem525@gmail.com>
2023-08-28 07:51:42 -0700

    done

---
+++ 0x18-dynamic_libraries/calculator.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * calculator - includes five calculator functions
+ * @a: first number
+ * @b: second number
+ * Return: the result of each equation
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+int sub(int a, int b)
+{
+        return (a - b);
+}
+int mul(int a, int b)
+{
+        return (a * b);
+}
+int div(int a, int b)
+{
+        return (a / b);
+}
+int mod(int a, int b)
+{
+        return (a % b);
+}
